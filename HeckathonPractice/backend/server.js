f5e6d052d4fc315b37ca52031c321c72cddddf63
harshp1624 <harshpra1624@gmail.com>
2025-12-31 12:37:28 +0530

    Working To-Do Manager backend with CRUD APIs

--- HeckathonPractice/backend/server.js
+++ HeckathonPractice/backend/server.js
@@ -2,13 +2,57 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Temporary storage
+let todos = [];
+
+// Test route
 app.get("/", (req, res) => {
-  res.send("Backend is running successfully");
+  res.send("To-Do Backend Running");
 });
 
+// Create todo
+app.post("/todos", (req, res) => {
+  if (!req.body || !req.body.title) {
+    return res.status(400).json({ error: "Title is required" });
+  }
+
+  const todo = {
+    id: Date.now(),
+    title: req.body.title,
+    completed: false,
+  };
+
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+// Get all todos
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+// Update todo
+app.put("/todos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  todos = todos.map((todo) =>
+    todo.id === id ? { ...todo, completed: req.body.completed } : todo
+  );
+  res.json({ message: "Todo updated" });
+});
+
+// Delete todo
+app.delete("/todos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  todos = todos.filter((todo) => todo.id !== id);
+  res.json({ message: "Todo deleted" });
+});
+
+// Start server
 app.listen(5000, () => {
-  console.log("Server started on port 5000");
+  console.log("Server running on http://localhost:5000");
 });
